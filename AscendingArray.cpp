6e1148b7d999086c2e8545e9c1139cd09f3c049d
Ankit <ankitmainali@isu.edu>
2025-04-02 09:42:00 -0600

    Project

---
+++ AscendingArray.cpp
@@ -0,0 +1,24 @@
+/* wap to sort the array in ascending order using an array and then decending*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int size;
+
+	cout<<"Enter the size of the array:"<<endl;
+	cin>>size;
+	
+	int array[size];
+
+/*for(int i=1;i<=size;i++){
+    cout<<i<<endl;
+}*/
+ for (int i = size; i >0 ; i--)
+ {
+     /* code */
+    cout<<i<<endl;
+ }
+    return 0;
+}
