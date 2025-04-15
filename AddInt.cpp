4670c86f9e1d98a4caaec949712135573ae5cee8
Ankit <ankitmainali@isu.edu>
2025-04-14 22:06:05 -0600

    practice

---
+++ AddInt.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout<<"Enter 1st integer:"<<endl;
+        cin>>num1;
+        cout<<"Enter 2nd integer:"<<endl;
+        cin>>num2;
+        
+        cout<<num1+num2;
+	return 0;
+}
